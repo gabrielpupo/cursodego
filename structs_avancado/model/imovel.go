0341256083f3472237dd6f716dc0d314839097e8
gabriel_pupo <gabriel.moraes@luizalabs.com>
2018-03-13 06:42:44 -0300

    aula 12

---
+++ structs_avancado/model/imovel.go
@@ -0,0 +1,13 @@
+package model
+
+//Imovel representa informações de um imóvel
+type Imovel struct {
+	X     int
+	Y     int
+	Nome  string
+	valor int
+}
+
+func (i *Imovel) SetValor(valor int) {
+
+}
