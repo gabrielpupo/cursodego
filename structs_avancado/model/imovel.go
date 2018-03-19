cad8ae2e4ba854b5429fc4d49223bd9ed0c00454
gabriel_moraes <gabriel.moraes@luizalabs.com>
2018-03-19 08:56:27 -0300

    aula 14

--- structs_avancado/model/imovel.go
+++ structs_avancado/model/imovel.go
@@ -2,12 +2,18 @@ package model
 
 //Imovel representa informações de um imóvel
 type Imovel struct {
-	X     int
-	Y     int
-	Nome  string
+	X     int    `json:"coordenada_x"`
+	Y     int    `json:"coordenada_y"`
+	Nome  string `json:"nome"`
 	valor int
 }
 
+//SetValor define o valor de um imóvel
 func (i *Imovel) SetValor(valor int) {
+	i.valor = valor
+}
 
+//GetValor retorna o valor de um imóvel
+func (i *Imovel) GetValor() int {
+	return i.valor
 }
